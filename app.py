06752b0d7a54ca6f3a56417495be464913c9b8d4
Hunaid2000 <hunaid2000@gmail.com>
2023-03-22 17:49:36 +0500

    Added model integration

--- app.py
+++ app.py
@@ -1,47 +1,38 @@
-import json
 from datetime import datetime
 import requests
-import plotly.graph_objs as go
-import plotly.express as px
-import plotly
 from flask import Flask, render_template, request, jsonify
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+import numpy as np
+
 app = Flask(__name__)
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     symbol = 'BTCUSDT'
-#     interval = '1m'
-#     limit = 30
-#     url = f'https://api.binance.com/api/v3/klines?symbol={symbol}&interval={interval}'
-#     response = requests.get(url)
-#     data = response.json()
-#     x = []
-#     open_values = []
-#     high_values = []
-#     low_values = []
-#     close_values = []
-#     for candle in data:
-#         x.append(datetime.fromtimestamp(candle[0]/1000))
-#         open_values.append(candle[1])
-#         high_values.append(candle[2])
-#         low_values.append(candle[3])
-#         close_values.append(candle[4])
-#     candlestick = go.Candlestick(x=x, open=open_values, high=high_values, low=low_values, close=close_values)
-#     data = [candlestick]
-#     layout = go.Layout(title=symbol)
-#     fig = go.Figure(data=data, layout=layout)
-#     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-#     return render_template('index.html', graphJSON=graphJSON)
-# if __name__ == '__main__':
-#     app.run(debug=True)
+
+close_values = []
+open_price = 0
+high_price = 0
+low_price = 0
+volumeto = 0
+volumefrom = 0
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        date = request.form['date']
-        open_price = request.form['open']
-        high_price = request.form['high']
-        low_price = request.form['low']
-        close_price = request.form['close']
-        print(f"{date}, {open_price}, {high_price}, {low_price}, {close_price}")
+    # load tensorflow model
+    model = tf.keras.models.load_model('btc_model.h5')
+
+    # Handle form submission
+    if request.method == 'POST':        
+        date = request.form['date']       
+        scaler = MinMaxScaler()
+        date_ordinal = datetime.strptime(date, '%Y-%m-%d').toordinal()
+        features = [[high_price, low_price, open_price, volumefrom, volumeto, date_ordinal]]        
+        features_scaled = scaler.fit_transform(features)        
+        # Make prediction using model
+        prediction = model.predict(features_scaled)
+        close_values_scaled = scaler.fit_transform(np.array(close_values).reshape(-1, 1))
+        prediction = close_values_scaled[-1] + prediction
+        prediction = scaler.inverse_transform(prediction)
+        prediction = prediction[0][0]
+        return render_template('index.html', prediction=prediction)
     return render_template('index.html')
 
 @app.route('/get_data')
@@ -51,6 +42,14 @@ def get_data():
     limit = 30
     url = f'https://api.binance.com/api/v3/klines?symbol={symbol}&interval={interval}&limit={limit}'
     response = requests.get(url)
+    for candle in response.json():
+        close_values.append(candle[4])
+        open_price = candle[1]
+        high_price = candle[2]
+        low_price = candle[3]
+        volumeto = candle[5]
+        volumefrom = candle[7]
+        
     data = response.json()
     return jsonify(data)
 
